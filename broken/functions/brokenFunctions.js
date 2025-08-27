3c3f6bbfb46a4a6fa6f30fdb436099ec2d65167d
Caleb Foss <caleb.foss@jjc.edu>
2025-08-27 13:34:51 -0500

    Add broken functions exercise

---
+++ broken/functions/brokenFunctions.js
@@ -0,0 +1,136 @@
+/* Becky
+
+Parameters:
+numberA - number
+numberB - number
+
+Subtracts numberB from numberA and returns the result.
+*/
+function subtract(numberA, numberB) {
+    numberA - numberB;
+}
+
+/* Ben
+
+Parameters:
+numberA - number
+numberB - number
+
+Divides numberA by numberB and returns the result.
+*/
+function divide(numberA, numberB) {
+    return numberB // numberA;
+}
+
+/* Eduardo
+
+Parameters:
+num - number
+
+Returns true if num is an odd number; returns false if not.
+*/
+function isOdd(num) {
+    return num % 1 === 0;
+}
+
+/* Kevin
+
+Parameters:
+numberA - number
+numberB - number
+
+Returns true if numberA is less than numberB; returns false if not.
+*/
+function isLessThan(numberA, numberB) {
+    return numbreA < numberB;
+}
+
+/* Mely
+
+Parameters:
+a - string
+b - string
+
+Concatenates stringB to the end of stringA and returns the result.
+*/
+function combine(stringA, stringB) {
+    return stringA + + stringB;
+}
+
+/* Michael
+
+Parameters:
+valueA - any data type
+valueB - any data type
+
+Returns true if valueA is equal to valueB (same data type and value); 
+returns false if not.
+*/
+function equal(valueA, valueB) {
+    return valueA = valueB;
+}
+
+/* Michelle
+
+Parameters:
+valueA - any data type
+valueB - any data type
+
+Returns true if valueA is not equal to valueB (different data type or value); 
+returns false if not.
+*/
+function notEqual(valueA, valueB) {
+    return; valueA !== valueB;
+}
+
+
+/* Rock
+
+Parameters:
+numberA - number
+numberB - number
+
+Multiplies numberA by numberB and returns the result.
+*/
+function multiply(numberA, numberB) {
+    return numberA ** numberB;
+}
+
+/* Sophia
+
+Parameters:
+numberA - number
+numberB - number
+
+Returns true if numberA is greater than numberB;
+returns false if not.
+*/
+function isGreaterThan() {
+    return numberA > numberB;
+}
+
+/* Victor
+
+Parameters:
+str - string
+
+Uses concatenation to sandwich the given string between the
+words "before" and "after", and returns the resulting string. 
+*/
+function sandwich(str) {
+    return "before' + str + 'after";
+}
+
+/* Zander
+
+Parameters:
+numberA - number
+numberB - number
+numberC - number
+
+Returns the mean (average) of numberA, numberB, and numberC
+by dividing their sum by 3.
+*/
+function mean(numberA, numberB, numberC) {
+    return numberA + numberB + numberC / 3;
+}
