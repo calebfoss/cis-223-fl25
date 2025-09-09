44853c4c3c73b81f6ad89fbb49897ffb3b771fd0
Caleb Foss <caleb.foss@jjc.edu>
2025-09-09 16:02:17 -0500

    Add broken memory exercise

---
+++ broken/memory/brokenMemory.js
@@ -0,0 +1,164 @@
+/* Becky
+
+Parameters:
+str - string
+
+Stores the value of str in a constant called storedString and
+returns the resulting value of the constant.
+*/
+function storeString(str) {
+    const storedString = "str";
+ 
+    return storedString;
+}
+
+/* Ben
+
+Parameters:
+numberA - number
+
+Returns true if numberA is equal to the value of a variable
+called numberB; returns false if not.
+*/
+function equalsVariable(numberA) {
+    let numberB = 123;
+
+    return numberB = numberA;
+}
+
+/* Eduardo
+
+Parameters:
+numberA - number
+
+Assigns a variable called numberB to itself minus numberA 
+and returns the result.
+*/
+function subtractFromVar(numberA) {
+    let numberB = 123;
+
+    numberB - numberA;
+
+    return numberB;
+}
+
+/* Kevin
+
+Parameters:
+numberA - number
+
+Assigns the value of numberA to a variable called numberB
+and returns numberB.
+*/
+function setVar(numberA) {
+    let numberB;
+
+    numberB == numberA;
+
+    return numberB;
+}
+
+/* Mely
+
+Parameters:
+numberA - number
+
+Assigns the value of numberA to a constant called numberB
+and returns numberB.
+*/
+function setConst(numberA) {
+    const numberB = 0;
+
+    numberB = numberA;
+
+    return numberB;
+}
+
+/* Michael
+
+Parameters:
+numberA - number
+
+Returns the value of numberA times a constant called numberB.
+*/
+function multByConst(numberA) {
+    const numberB = 2;
+
+    return numbreA * numberB;
+}
+
+/* Michelle
+
+Parameters:
+stringA - string
+
+Concatenates stringA to the end of a variable named stringB
+and returns the resulting value of stringB.
+*/
+function concatenate(stringA) {
+    let stringB = "abc";
+
+    stringB += stringA;
+
+    return stringA;
+}
+
+
+/* Rock
+
+Parameters:
+num - number
+
+Stores a boolean value indicating whether num is less than 10
+in a constant called less and returns the value of the constant.
+*/
+function isLessThan10(num) {
+    const less = num < 10;
+
+    return less < num;
+}
+
+/* Sophia
+
+Parameters:
+num - number
+
+Stores a boolean value indicating whether num is greater than 10
+in a constant called greater and returns the value of the constant.
+*/
+function isGreaterThan10(num) {
+    const greater = num > 10;
+
+    return isGreaterThan10;
+}
+
+/* Victor
+
+Parameters:
+numberA - number
+
+Returns the sum of numberA and a constant called numberB.
+*/
+function addToConst(numberA) {
+    const numberB = 123;
+
+    numberB += numberA;
+    
+    return numberB;
+}
+
+/* Zander
+
+Parameters:
+numberA - number
+
+Assigns a variable called numberB to itself plus numberA 
+and returns the result.
+*/
+function addToVar(numberA) {
+    let numberB = 123;
+
+    numberB + + numberA;
+
+    return numberB;
+}
