8a38f86d1982150ff86b4574ac9c84538603ac6b
cachech-sketch <cachech@gmail.com>
2025-09-09 20:37:03 -0500

    add varibale for how manny times flower was water

--- classProject/script.js
+++ classProject/script.js
@@ -2,7 +2,8 @@
 // MEMORY INITIALIZATION
 
 // Becky
-
+//Initialize a variable to store the number of times the current flower has been watered using an initial value of 0.
+timesWatered = 0;
 // Ben
 
 // Eduardo
