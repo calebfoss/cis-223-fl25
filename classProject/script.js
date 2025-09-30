ce5ef326c86b50a0793beb40a0eabf1685fa4d7c
cachech-sketch <cachech@gmail.com>
2025-09-29 20:53:45 -0500

    update

--- classProject/script.js
+++ classProject/script.js
@@ -13,8 +13,9 @@ import {
 let timesWatered = 0;
 //Write a function that checks how many times the current flower has been watered (timesWatered variable). If the number is equal to the waterCap, place a flower image at the top of the stem.
 function myCheck(){  timesWatered=0
-    if (timesWatered =waterCap)
-        return console.log ("you have  grown  a flower!")
+    if (timesWatered ===waterCap)
+       return anchor.y.flower=20
+        // return console.log ("you have  grown  a flower!")
     // i dont know how  to add a flower
 
     
