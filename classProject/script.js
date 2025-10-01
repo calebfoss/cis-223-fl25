6c747fc96385d89865b056601f7c93a42a8da32e
michelledu12 <duahmichelle2@icloud.com>
2025-10-01 11:13:03 -0500

    Add function to drop seed from random position above canvas.

--- classProject/script.js
+++ classProject/script.js
@@ -14,7 +14,14 @@
 // Michael
 
 // Michelle
+function buySeed() 
+{
+    seed.anchor.x = Math.random() * canvas.width;   // Set the seed's anchor to a random X position.
+    
+    seed.anchor.y = -seed.height;   // Set Y position just above the canvas.
 
+    seed.velocity.y = -2; // Set the seed's velocity to move straight down,
+}
 // Rock
 
 // Sophia
