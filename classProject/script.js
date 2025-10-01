51a69095f6963af4307f738d74268817a38fddbf
michelledu12 <duahmichelle2@icloud.com>
2025-10-01 11:13:03 -0500

    Add function to drop seed from random position above canvas.

--- classProject/script.js
+++ classProject/script.js
@@ -116,14 +116,13 @@ const moneyDisplay = canvas.text({
 
 // Michelle
 
-function purchaseSeed() 
-{
-    seed.anchor.x = Math.random() * canvas.width;   // Set the seed's anchor to a random X position.
-    
-    seed.anchor.y = -seed.height;   // Set Y position just above the canvas.
+function purchaseSeed() {
+  seed.anchor.x = Math.random() * canvas.width; // Set the seed's anchor to a random X position.
 
-    seed.velocity = Vector2D.xy(0, 2);
-     // Set the seed's velocity to move straight down,
+  seed.anchor.y = -seed.height; // Set Y position just above the canvas.
+
+  seed.velocity = Vector2D.xy(0, 2);
+  // Set the seed's velocity to move straight down,
 }
 
 // When this button is pressed
