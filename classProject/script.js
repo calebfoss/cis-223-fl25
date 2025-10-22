6a21a64453b06eedb7e97dd232fb0ae43b8921bb
Caleb Foss <caleb.foss@jjc.edu>
2025-10-22 14:58:45 -0500

    Monday 10/20 in class

--- classProject/script.js
+++ classProject/script.js
@@ -1,11 +1,14 @@
 import { createRoot, Color, Vector2D, Angle, Random } from "web-spinner";
 
+// Variation on flower height
+//  chance-based
+
 /////////////////////////////
 // MEMORY INITIALIZATION
 
 let timesWatered = 0;
 
-const waterCap = 3;
+const waterCap = 5;
 
 let selectedFlowerSeed = "";
 
@@ -15,33 +18,7 @@ let flowerVerticalLocation = -1;
 
 const waterButton = document.createElement("button");
 
-function buySeed() {
-  const validColors = ["blue", "orange", "pink", "purple"];
-  let selectedColor = "";
 
-  while (true) {
-    let input = prompt(
-      "What color of flower would you like to buy? (blue, orange, pink, purple)"
-    );
-    if (input === null) {
-      alert("Purchase cancelled.");
-      return; // User cancelled prompt
-    }
-
-    input = input.trim().toLowerCase();
-
-    if (validColors.includes(input)) {
-      selectedColor = input;
-      alert(`You bought a ${selectedColor} flower seed.`);
-      break;
-    } else {
-      alert(`Sorry, we're out of ${input} flower seeds.`);
-    }
-  }
-
-  // `selectedColor` used for further logic here
-  return selectedColor;
-}
 
 // Victor
 let seedPositionX = -1;
@@ -73,15 +50,10 @@ const seed = canvas.ellipse({
   height: 20,
   fill: "tan",
   stroke: "none",
-  anchor: Vector2D.xy(100, 100),
+  anchor: Vector2D.xy(0, -20),
 });
 
-// Create flower image
-const flower = canvas.image({
-  source: "images/FLower 5/Flower 5 - BLUE.png",
-  scale: 4,
-  anchor: Vector2D.xy(20, groundLevel - 100),
-});
+
 
 // Create rectangle for ground
 const ground = canvas.rectangle({
@@ -91,6 +63,8 @@ const ground = canvas.rectangle({
   width: canvas.width,
   height: canvas.height - groundLevel,
 });
+
+const stems = [];
 
 // Turn off image smoothing for pixel art
 // (otherwise it will look blurry)
@@ -114,39 +88,103 @@ const moneyDisplay = canvas.text({
 /////////////////////////////
 // GAME
 
+function modifyMoney(amount) {
+  playerAmount += amount;
+
+  moneyDisplay.textContent = "$" + playerAmount;
+}
+
+function buySeed() {
+  const validColors = ["blue", "orange", "pink", "purple"];
+  let selectedColor = "";
+
+  while (true) {
+    let input = prompt(
+      "What color of flower would you like to buy? (blue, orange, pink, purple)"
+    );
+    if (input === null) {
+      alert("Purchase cancelled.");
+      return; // User cancelled prompt
+    }
+
+    input = input.trim().toLowerCase();
+
+    if (validColors.includes(input)) {
+      selectedColor = input;
+   
+
+      break;
+    } else {
+      alert(`Sorry, we're out of ${input} flower seeds.`);
+    }
+  }
+
+        // `selectedColor` used for further logic here
+        selectedFlowerSeed = selectedColor;
+
+    const price = seedPrice(selectedFlowerSeed);
+
+      if(playerAmount < price) {
+        alert("Sorry, not enough cash!");
+
+        return;
+      }
+
+      alert(`You bought a ${selectedColor} flower seed.`);
+
+
+
+      
+
+      modifyMoney(-price);
+
+      purchaseSeed();
+}
+
 // Michelle
 
 function purchaseSeed() {
-  seed.anchor.x = Math.random() * canvas.width; // Set the seed's anchor to a random X position.
-
+  seedPositionX = Math.random() * canvas.width; // Set the seed's anchor to a random X position.
+  seed.anchor.x = seedPositionX; 
   seed.anchor.y = -seed.height; // Set Y position just above the canvas.
 
-  seed.velocity = Vector2D.xy(0, 2);
+  seed.velocity = Vector2D.xy(0, 200);
   // Set the seed's velocity to move straight down,
 }
 
 // When this button is pressed
 // call the purchaseSeed function
-buySeedButton.addEventListener("click", purchaseSeed);
+buySeedButton.addEventListener("click", buySeed);
 
 // Benjamin
 
-const stem = canvas.rectangle({
-  width: 10,
-  height: 20,
-  fill: 'green',
-  stroke: 'none',
-  anchor: Vector2D.xy(105, 100)
-});
+
+
+function grow() {
+  const stemHeight = 20;
+
+  const stem = canvas.rectangle({
+    width: 10,
+    height: stemHeight,
+    fill: 'green',
+    stroke: 'none',
+    anchor: Vector2D.xy(seedPositionX, groundLevel - stemHeight * timesWatered)
+  });
+
+  // Put the new stem in the list of stems
+  stems.push(stem);
+}
 
 function waterFlower() {
   
   timesWatered += 1;
+
   if (timesWatered >= waterCap) {
     bloom();
     timesWatered = 0;
+  } else {
+    grow();
   }
-  alert(timesWatered);
 }
 
 waterButton.addEventListener("click", waterFlower);
@@ -155,39 +193,51 @@ waterButton.addEventListener("click", waterFlower);
 
 function bloom() {
   if (timesWatered === waterCap) {
-    flower.anchor.y = 20;
+    // Create flower image
+    const flower = canvas.image({
+      source: "images/FLower 5/Flower 5 - " + selectedFlowerSeed.toUpperCase() + ".png",
+      scale: 4,
+    });
 
-    // Make money from flower blooming
-    flowerPassiveIncome();
+    flower.anchor = Vector2D.xy(seedPositionX - flower.width / 2, groundLevel - 100);
+
+    // Remove all of the stems
+    while(stems.length > 0) {
+      // Get last stem in list
+      const lastStem = stems.pop();
+      // Remove it
+      lastStem.remove();
+    }
+
+    function removeThisFlower() {
+      removeFlower(flower);
+    }
+
+    flower.addEventListener("click", removeThisFlower);
   }
 }
 
-// Rock
-function onFlowerBloom(playerAmount, flowerPrice) {
-  playerAmount += flowerPrice;
-}
-
-// Sophia
-
-function flowerPassiveIncome() {}
-
 // Kevin
 
-function removeFlower() {
+function removeFlower(flower) {
     
     const confirmRemoval = confirm("Do you want to remove this flower?");
     
     if (confirmRemoval) {
         flower.remove();
-        playerAmount += flowerPrice;
+
+        const flowerPrice = setFlowerPrice(selectedFlowerSeed);
+
+        modifyMoney(flowerPrice);
     }
 }
 
 // Mely
 
-let flowerPrice = 0;
 
 function setFlowerPrice(flowerColor) {
+  let flowerPrice = 0;
+
   if (flowerColor === "blue") {
     flowerPrice = 9;
   } else if (flowerColor === "orange") {
@@ -201,8 +251,6 @@ function setFlowerPrice(flowerColor) {
   }
   return flowerPrice;
 }
-
-setFlowerPrice();
 
 // Eduardo
 
